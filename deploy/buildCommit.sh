6ba21776e6e8885607e32683451b0fc8ea802c5d
Jason Holloway <miceeatnicerice@hotmail.com>
2017-08-06 05:21:59 +0100

    improvements to store build script

--- deploy/buildCommit.sh
+++ deploy/buildCommit.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+LAMBDA_NAME=bb-store
+IMAGE_KEY=bb/lambda/store-0.1.jar
+JAR_PATH=/mnt/c/dev/bb/store/target/scala-2.12/bb-store-assembly-0.1-SNAPSHOT.jar
+
+set +x
 
 main() {
-    pushd ../commit
+    pushd ../store
     build
-    if[ publish ] then; 
-        switchLambda
+    if [[ $(publish) ]] 
+        then updateLambda
     fi
     popd
 }
@@ -15,12 +20,16 @@ build() {
 
 publish() {
     aws s3api put-object \
-        --bucket ${IMAGE_BUCKET} \
-        --key commit/${IMAGE_NAME} \
-        --body $IMAGE_PATH
+        --bucket $IMAGE_BUCKET \
+        --key $IMAGE_KEY \
+        --body $JAR_PATH
 }
 
-switchLambda() {
+updateLambda() {
+    aws lambda update-function-code \
+        --function-name $LAMBDA_NAME \
+        --s3-bucket $IMAGE_BUCKET \
+        --s3-key $IMAGE_KEY
 }
 
 main
